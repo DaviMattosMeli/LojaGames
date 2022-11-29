16f98b992df328f976f14e3a1961b1b758573edd
DaviMattosMeli <davi.mattos@mercadolivre.com>
2022-11-29 16:21:16 -0300

    Adicao @Repository no CategoriaRepository

--- src/main/java/com/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/lojagames/repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.lojagames.repository;
 import com.lojagames.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
     public List<Categoria> findAllByTipoContainingIgnoreCaseOrderByTipo(@Param("tipo") String tipo);
